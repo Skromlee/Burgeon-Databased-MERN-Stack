c195d23c36641da98d9a42c72e0da66ba2015bbd
Skromlee <suppawat.kromlee@m.swu.ac.th>
2022-11-23 10:28:32 +0700

    Up date todo

--- TODO.js
+++ TODO.js
@@ -1,7 +1,13 @@
 // !FIX Placeholder in any inputbox
 // !FIX Delete Dialog to specific name in all size
 
-// !TODO Make the delete work in group
+// // !TODO Make the delete work in group
 // !TODO Make the global Parcel Id Search
-// !TODO Make the user Parcel Id Indetail with Spectified Image
+// // !TODO Make the user Parcel Id Indetail with Spectified Image
 // !TODO IF Have a time Care about
+
+//!TODO Check Group Model => Change Inside Array to Object ID Type
+//!TODO Make Search parcel pages
+//!TODO Make Search box Query information
+
+//!TODO In Create Group Check Button label
