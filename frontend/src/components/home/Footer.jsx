6016edc7d2fee3c4aab7270e77b1ab9b82b4c3b3
Skromlee <suppawat.kromlee@m.swu.ac.th>
2022-11-26 21:24:40 +0700

    update footer

--- frontend/src/components/home/Footer.jsx
+++ frontend/src/components/home/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                     >
                         Admin Section
                     </Link>
-                    <div className="mx-auto my-6 text-center text-white md:hidden">
+                    <div className="mx-auto my-6 text-center text-white md:hidden  items-center">
                         Copyright &copy; 2022, All Rights Reserved
                     </div>
                     {/* <!-- Logo --> */}
@@ -24,89 +24,20 @@ const Footer = () => {
                             alt="Burgeon Logo"
                             className="max-h-10"
                         />
-                        <h1 className="text-2xl text-white font-medium">
+                        <h1 className="text-2xl text-white font-medium  items-center">
                             BURGEON
                         </h1>
-                    </div>
-
-                    {/* <!-- social Links container --> */}
-                    <div className="flex justify-center space-x-4">
-                        {/* <!-- Link1 --> */}
-                        <Link href="">
-                            <img
-                                src="images/icon-facebook.svg"
-                                alt=""
-                                className="h-8"
-                            />
-                        </Link>
-                        {/* <!-- Link2 --> */}
-                        <Link href="">
-                            <img
-                                src="images/icon-youtube.svg"
-                                alt=""
-                                className="h-8"
-                            />
-                        </Link>
-                        {/* <!-- Link3 --> */}
-                        <Link href="">
-                            <img
-                                src="images/icon-twitter.svg"
-                                alt=""
-                                className="h-8"
-                            />
-                        </Link>
-                        {/* <!-- Link4 --> */}
-                        <Link href="">
-                            <img
-                                src="images/icon-pinterest.svg"
-                                alt=""
-                                className="h-8"
-                            />
-                        </Link>
-                        {/* <!-- Link5 --> */}
-                        <Link href="">
-                            <img
-                                src="images/icon-instagram.svg"
-                                alt=""
-                                className="h-8"
-                            />
-                        </Link>
                     </div>
                 </div>
 
                 {/* <!-- List Container --> */}
                 <div className="flex justify-around space-x-32">
-                    {/* <div className="flex flex-col space-y-3 text-white">
-                        <Link href="#" className="hover:text-brightRed">
-                            Home
-                        </Link>
-                        <Link href="#" className="hover:text-brightRed">
-                            About
-                        </Link>
-                        <Link href="#" className="hover:text-brightRed">
-                            Our Teams
-                        </Link>
-                        <Link href="#" className="hover:text-brightRed">
-                            My Customers
-                        </Link>
+                    <div className="hidden text-white md:block align-middle my-auto">
+                        Copyright &copy; 2022, All Rights Reserved
                     </div>
-                    <div className="flex flex-col space-y-3 text-white">
-                        <Link href="#" className="hover:text-brightRed">
-                            Careers
-                        </Link>
-                        <Link href="#" className="hover:text-brightRed">
-                            Community
-                        </Link>
-                        <Link href="#" className="hover:text-brightRed">
-                            Privacy Policy
-                        </Link>
-                    </div> */}
                 </div>
                 {/* <!-- Input Container --> */}
                 <div className="flex flex-col justify-center items-center">
-                    <div className="hidden text-white md:block">
-                        Copyright &copy; 2022, All Rights Reserved
-                    </div>
                     <Link
                         to="/admin"
                         className="hidden md:block px-6 py-2 text-sm text-slate-500"
